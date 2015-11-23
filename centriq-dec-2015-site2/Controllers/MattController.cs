f0e7987b628381eccebd0e3d8aefc9565a0cc503
Jake Lunn <jakelunn93@gmail.com>
2015-11-23 12:04:12 -0600

    updated Matt's bio, added Matt controller

---
+++ centriq-dec-2015-site2/Controllers/MattController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace centriq_dec_2015_site2.Controllers
+{
+    public class MattController : Controller
+    {
+        // GET: Matt
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Bio()
+        {
+            return View();
+        }
+    }
+}
